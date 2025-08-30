8981c8910a17661f4117142766fd2854cd49a10d
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-08-30 14:39:53 +0530

    create layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,52 @@
+import { Bell, HelpCircle, MenuIcon, Search} from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-neutral-50">
+      <header className="w-full bg-white shadow px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <h1 className="text-2xl font-bold text-primary">Demo</h1>
+          <h1 className="text-2xl ">App</h1>
+        </div>
+
+        <div className="hidden md:flex items-center gap-4">
+          <Button className="p-2 rounded-full hover:bg-neutral-100">
+            <Search className="w-5 h-5 text-neutral-600" />
+          </Button>
+          <Button className="p-2 rounded-full hover:bg-neutral-100">
+            <HelpCircle className="w-5 h-5 text-neutral-600" />
+          </Button>
+          <Button className="p-2 rounded-full hover:bg-neutral-100">
+            <Bell className="w-5 h-5 text-neutral-600" />
+          </Button>
+        </div>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <Button
+                variant="outline"
+                className="bg-white border border-primary"
+              >
+                {<MenuIcon className="text-primary" />}
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <div className="flex items-center">
+                  <h1 className="text-2xl font-bold text-primary">Demo</h1>
+                  <h1 className="text-2xl ">App</h1>
+                </div>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </header>
+
+      <main className="flex-1 grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
+        {children}
+      </main>
+    </div>
+  );
+}
