cdacecde18a5de09791e508eab98fc5df0993d3d
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-08-31 14:23:42 +0530

    use 3 components in the dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,28 +1,14 @@
-
-import { Card } from "@/components/ui/card";
-import PipelineCard from "@/components/pipeline/PipelineCard";
-
-
+import BorrowerDetailCard from "@/components/borrower/BorrowerDetailCard";
+import BrokerOverview from "@/components/broker/BrokerOverview";
+import BorrowerPipeline from "@/components/pipeline/BorrowerPipeline";
 
 function Dashboard() {
- 
-
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <PipelineCard/>
-        <Card className="w-full p-2">
-          <h2 className="text-xl font-bold">Borrower</h2>
-          <p className="text-sm text-muted-foreground">
-            Card description goes here.
-          </p>
-        </Card>
-        <Card className="w-full p-2">
-          <h2 className="text-xl font-bold">Brocker</h2>
-          <p className="text-sm text-muted-foreground">
-            Card description goes here.
-          </p>
-        </Card>
+        <BorrowerPipeline />
+        <BorrowerDetailCard />
+        <BrokerOverview />
       </div>
     </>
   );
